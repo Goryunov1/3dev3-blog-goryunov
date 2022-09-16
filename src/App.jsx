d9d604a9b0b69161639a4bd5bba722cb62c035d4
Goryunov1 <sasha.goryunov.98@mail.ru>
2022-09-16 15:52:02 +0300

    card

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 import Header from "./components/Header";
-import Main from "./components/Main"
-import Footer from "./components/Footer"
-
+import Main from "./components/Main";
+import Footer from "./components/Footer";
+import posts from "./data/posts.json";
 export default () => {
     
     return <>
         <Header/>
-        <Main/>
+        <Main data={posts}/>
         <Footer/>
-        {/* <h1>Goryunov Blog!</h1> */}
     </>
 }
--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,9 +1,21 @@
 import React from "react";
 import "./style.css";
 
-export default () => {
+export default ({data}) => {
     return <main>
         <h1>Devs Blog</h1>
-        <h2>Gryunov</h2>
+        <div className="cards">
+            {data.map(post => <div className="post" key={post.name}>
+                <div className="post__block">
+                    <span>{post.name}</span>
+                    <span>{post.author}</span>
+                </div>
+                <div className="post__block">
+                    {post.description.split(".").map((p, i,
+                         arr) => i !== arr.length -1 &&<p key=
+                         {i}>{p + "."}</p>)}
+                </div>
+            </div>)}
+        </div>
     </main>
 }
